1e938554edc05699f27d2d17ad88a4595ab56c37
ZooRoooo <indrajitd2002@gmail.com>
2025-08-30 13:06:53 +0530

    30/8/25

--- controllers/site.controller.js
+++ controllers/site.controller.js
@@ -13,20 +13,67 @@ const index=async(req,res)=>{
     .populate('author','fullname')
     .sort({createdAt:-1})
     .limit(5);
-    //res.json({news});
-    res.render('index',{news});
+
+    const categoriesInUse=await NewsModel.distinct('category');
+    const categories=await CategoryModel.find({'_id':{$in:categoriesInUse}});
+    
+    //res.json({news,categories});
+
+    
+    res.render('index',{news,categories});
 }
 const articleBycategory=async(req,res)=>{
-    res.render('category');
+   const category = await CategoryModel.findOne({ slug: req.params.name });
+
+    if(!category){
+        return res.status(400).send("No category Found");
+    }
+     const news=await NewsModel.find({category:category._id})
+    .populate('category',{'name':1,'slug':1})
+    .populate('author','fullname')
+    .sort({createdAt:-1})
+    .limit(5);
+
+    const categoriesInUse=await NewsModel.distinct('category');
+    const categories=await CategoryModel.find({'_id':{$in:categoriesInUse}});
+
+    res.render('category',{news,categories});
 }
 const singleArticle=async(req,res)=>{
-    res.render('single');
+   const news=await NewsModel.find({})
+    .populate('category',{'name':1,'slug':1})
+    .populate('author','fullname')
+    .sort({createdAt:-1})
+    .limit(5);
+
+    const categoriesInUse=await NewsModel.distinct('category');
+    const categories=await CategoryModel.find({'_id':{$in:categoriesInUse}});
+
+    res.render('single',{news,categories});
 }
 const search=async(req,res)=>{
-    res.render('search');
+    const singleNews=await NewsModel.findById(req.params.id)   
+    .populate('category',{'name':1,'slug':1})
+    .populate('author','fullname')
+    .sort({createdAt:-1})
+    .limit(5);
+
+    const categoriesInUse=await NewsModel.distinct('category');
+    const categories=await CategoryModel.find({'_id':{$in:categoriesInUse}});
+
+    res.render('search',{singleNews,categories});
 }
 const author=async(req,res)=>{
-    res.render('author');
+    const news=await NewsModel.find({author:req.params.name})
+    .populate('category',{'name':1,'slug':1})
+    .populate('author','fullname')
+    .sort({createdAt:-1})
+    .limit(5);
+
+    const categoriesInUse=await NewsModel.distinct('category');
+    const categories=await CategoryModel.find({'_id':{$in:categoriesInUse}});
+
+    res.render('author',{news,categories});
 }
 const addcomment=async(req,res)=>{
     
